c6c6022a9bec91bb673e06271cc702ee03db4730
root <ibm11@live.com>
2023-06-15 15:02:36 -0700

    New Update

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,10 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - A program that prints exactly with proper grammar, but the outcome is a piece of art, followed by a new line.
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
