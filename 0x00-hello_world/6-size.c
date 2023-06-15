bf3002c66eb1fc9f252e1d1d1191ef75c4130510
root <ibm11@live.com>
2023-06-15 15:31:15 -0700

    New Update

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,12 +2,11 @@
 /**                                                                                                                                                           * main - A program that prints exactly with proper grammar, but the outcome is a piece of art, followed by a new line.                                       * Return: 0 (Success)                                                                                                                                        */
 int main(void)
 {
-printf("Size of char: %zu byte(s)\n", sizeof(char));
-printf("Size of short: %zu byte(s)\n", sizeof(short));
-printf("Size of int: %zu byte(s)\n", sizeof(int));
-printf("Size of long: %zu byte(s)\n", sizeof(long));
-printf("Size of float: %zu byte(s)\n", sizeof(float));
-printf("Size of double: %zu byte(s)\n", sizeof(double));
-printf("Size of long double: %zu byte(s)\n", sizeof(long double));
-return (0);
+
+	printf("Size of a char: %i byte(s)\n", sizeof(char));
+	printf("Size of an int: %i byte(s)\n", sizeof(int));
+	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %i byte(s)\n", sizeof(float));
+	return (0);
 }
