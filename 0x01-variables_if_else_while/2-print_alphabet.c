8b74d59498a2a4669989f7204b7901a8f1107ecc
root <ibm11@live.com>
2023-06-16 16:40:30 -0700

    0x01. C - Variables, if, else, while Completed

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,17 @@
 #include <stdio.h>
+/**
+ * main - print the letters a - z lowercase
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+	putchar(letter);
+	letter++;
+	}
+	putchar('\n');
+	return 0;
+}
