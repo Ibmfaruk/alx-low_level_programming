9bdf4db10965bf2993932a08b6dd054b7f2fe969
root <ibm11@live.com>
2023-07-07 19:28:09 -0700

    Completed all 0x0A. C - argc, argv Mandatory and Advanced Tasks

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,18 +1,16 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
- * main - print the name of the file followed by a new line
+ * main - prints its name, followed by a new line.
  * @argc: argument count
- * @argv: arguments passed to main as an array of strings
+ * @argv: arguments
+ *
  * Return: 0
  */
-
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	(void) argc;
+	(void)argc;
 
 	printf("%s\n", argv[0]);
-
 	return (0);
 }
